0bb4b6d00b94bb19b63c5443797ce163830663c3
cn <814549195@qq.com>
2018-07-22 16:18:54 +0800

    cn

--- project/util.py
+++ project/util.py
@@ -8,6 +8,17 @@ def get_config(section,key):
 
 
 
-def get_up_postion(positon_number):
-
+def get_up_position(position_number):
+    if(position_number==1):
+        position="中专及中专以下"
+    elif(position_number==2):
+        position="大专"
+    elif(position_number==3):
+        position="本科"
+    elif(position_number==4):
+        position="硕士研究生"
+    elif(position_number==5):
+        position="博士研究生"
+    else:
+        position="其他"
     return position
