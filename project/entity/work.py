92254232f643645872cb27502b79b868a4183d98
cn <814549195@qq.com>
2018-07-22 19:19:30 +0800

    cn

--- project/entity/work.py
+++ project/entity/work.py
@@ -1,6 +1,10 @@
 class Work(object):
 
-    def __init__(self,company,position,time):
+    # def __init__(self,company,position,time):
+    #     self.company = company
+    #     self.position = position
+    #     self.time = time
+    def __init__(self,time,company,position):
         self.company = company
         self.position = position
         self.time = time
